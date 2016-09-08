cbfcf2da47e20e4f74b2a29bc6633f65d083ea78
GingerAebi <gingerAebi@gmail.com>
2016-09-08 18:41:26 +0900

    Service TEST

--- app/src/main/java/com/bridge4biz/laundry/cleanbasket_androidver2/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/bridge4biz/laundry/cleanbasket_androidver2/fcm/MyFirebaseMessagingService.java
@@ -1,5 +1,15 @@
 package com.bridge4biz.laundry.cleanbasket_androidver2.fcm;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+
+import com.bridge4biz.laundry.cleanbasket_androidver2.R;
+import com.bridge4biz.laundry.cleanbasket_androidver2.activities.SplashActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -9,7 +19,31 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        //TODO Notification으로 띄우기 
-        super.onMessageReceived(remoteMessage);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_order_pickupreality)
+                        .setContentTitle(remoteMessage.getData().get("title"))
+                        .setContentText(remoteMessage.getData().get("text"));
+
+        Intent resultIntent = new Intent(this, SplashActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(SplashActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
+
+
+        Log.d("", "From: " + remoteMessage.getFrom());
+        Log.d("", "Notification Message Body: " + remoteMessage.getNotification().getBody());
     }
 }
